c8184d0888c52543380f4cf891571692a42f591e
n-miller77 <146680703+n-miller77@users.noreply.github.com>
2025-03-03 11:49:09 -0500

    Update get_homologues_pace.sh

--- get_homologues_pace.sh
+++ get_homologues_pace.sh
@@ -1,1 +1,18 @@
-homologues.pl -d <directory> -m cluster -X -M -t 0
+#!/bin/bash
+#SBATCH -Jget_homologues
+#SBATCH --account=gts-sbrown365-paid
+#SBATCH -N1 --ntasks-per-node=1
+#SBATCH --cpus-per-task=20
+#SBATCH --mem-per-cpu=32G
+#SBATCH -t 2:00:00
+#SBATCH -q inferno
+#SBATCH -o Report-%j.out
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=nmiller304@gatech.edu
+
+
+source ~/.bashrc   # Ensure conda/mamba is available
+mamba activate gethomo
+
+
+homologues.pl -d homolo_test -m cluster -X -M -t 0

e618ad51f10fd7988abd1a54821317ef40d6b63d
n-miller77 <146680703+n-miller77@users.noreply.github.com>
2025-03-06 11:11:29 -0500

    Update Workflow_Combo.sh

--- Workflow_Combo.sh
+++ Workflow_Combo.sh
@@ -1,1 +1,12 @@
-alksjfdf
+# First need to download NCBI genomes
+# Make mamba env with augustus and gffread
+# Make mamba env with get_homologues 
+
+
+
+
+
+
+
+
+homologues.pl -d /storage/home/hcoda1/9/nmiller304/scratch/homolo_test -m cluster -X -M -t 0
